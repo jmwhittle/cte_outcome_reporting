915b7a8c25b95a719bd6333958a8731f7cb1f8f6
jmwhittle <jasonwhittle@tutanota.com>
2017-11-29 10:19:34 -0700

    prototype:
    -still needs SQL query development
    -needs to change GPA to annual income
    -move table to top
    -add data to folder

--- cte_outcomes_app/app.R
+++ cte_outcomes_app/app.R
@@ -1,50 +1,84 @@
-#
-# This is a Shiny web application. You can run the application by clicking
-# the 'Run App' button above.
-#
-# Find out more about building applications with Shiny here:
-#
-#    http://shiny.rstudio.com/
-#
-
 library(shiny)
+library(tidyverse); theme_set(theme_minimal())
+
+data <- read_csv('survey_pidms.csv') %>% filter(DEGREE_CODE != "APE")
+
+library(lubridate)
+data$GRADUATION_DATE <- dmy(data$GRADUATION_DATE)
+data <- data %>% filter(GRADUATION_DATE > "2012-01-01" & GRADUATION_DATE < "2016-12-30")
 
 # Define UI for application that draws a histogram
 ui <- fluidPage(
-   
-   # Application title
-   titlePanel("Old Faithful Geyser Data"),
-   
-   # Sidebar with a slider input for number of bins 
-   sidebarLayout(
-      sidebarPanel(
-         sliderInput("bins",
-                     "Number of bins:",
-                     min = 1,
-                     max = 50,
-                     value = 30)
-      ),
-      
-      # Show a plot of the generated distribution
-      mainPanel(
-         plotOutput("distPlot")
-      )
-   )
-)
+  titlePanel("SLCC CTE program and workforce outcomes"),
+  
+  sidebarLayout(
+    sidebarPanel(
+  selectInput(inputId = "college_ind", label = "College", multiple = FALSE, choices = data$COLLEGE),
+  uiOutput("program"),
+  uiOutput("award")
+  ),
+  
+  mainPanel(
+    plotOutput(outputId = "plot"),
+    plotOutput(outputId = "plot_ts"),
+    tableOutput(outputId = "table")
+            )
+)) 
 
 # Define server logic required to draw a histogram
 server <- function(input, output) {
-   
-   output$distPlot <- renderPlot({
-      # generate bins based on input$bins from ui.R
-      x    <- faithful[, 2] 
-      bins <- seq(min(x), max(x), length.out = input$bins + 1)
-      
-      # draw the histogram with the specified number of bins
-      hist(x, breaks = bins, col = 'darkgray', border = 'white')
-   })
-}
+  df0 <- eventReactive(input$college_ind, {
+    data %>% dplyr::filter(COLLEGE %in% input$college_ind)
+  })
 
+  output$program <- renderUI({
+    selectInput(inputId = "program", label = "Program", multiple = TRUE, choices = df0()$MAJOR_CODE_1)
+  })
+  
+  df1 <- eventReactive(input$program, {
+    df0() %>% dplyr::filter(MAJOR_CODE_1 %in% input$program)
+  })
+  
+  output$award <- renderUI({
+    selectInput(inputId = "award", label = "Award level", choices = df1()$DEGREE_CODE)
+  })
+
+  df2 <- eventReactive(input$award, {
+    df1() %>% dplyr::filter(DEGREE_CODE %in% input$award)
+  })
+  
+  output$plot <- renderPlot(df2() %>% 
+      ggplot() + 
+      geom_density(aes(CUM_INSTITUTION_GPA, color = MAJOR_CODE_1)) + 
+      geom_vline(xintercept = median(as.numeric(df2()$CUM_INSTITUTION_GPA))) +
+      geom_vline(xintercept = quantile(as.numeric(df2()$CUM_INSTITUTION_GPA), 0.25), linetype=3) + 
+      geom_vline(xintercept = quantile(as.numeric(df2()$CUM_INSTITUTION_GPA), 0.75), linetype=3) + 
+      ggtitle("Cummulative institutional GPA") + 
+      xlab("Cummulative GPA") + 
+      ylab("Density") + 
+      scale_color_discrete(name="Program")
+  )
+  
+  output$plot_ts <- renderPlot(df2() %>% 
+                                 group_by(TERM_CODE, MAJOR_CODE_1) %>% 
+                                 tally() %>%
+                                 ggplot() + 
+                                 geom_line(aes(x=TERM_CODE, y=n, color = MAJOR_CODE_1)) +
+                                 ggtitle("Number of Awards by program") +
+                                 xlab("Year (calendar)") + 
+                                 ylab("Number of Awards") + 
+                                 scale_color_discrete(name="Program")
+                               )
+  
+
+  output$table <- renderTable({df2() %>% group_by(MAJOR_CODE_1) %>% 
+    summarise('Median GPA' = median(CUM_INSTITUTION_GPA),
+              '25%' = quantile(as.numeric(CUM_INSTITUTION_GPA), 0.25),
+              '75%' = quantile(as.numeric(CUM_INSTITUTION_GPA), 0.75),
+              students = n_distinct(PIDM),
+              awards = n())
+  })
+}
 # Run the application 
 shinyApp(ui = ui, server = server)
 
